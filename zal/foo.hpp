f96e17a987ba6a3eb3bb6469fb3c7e6f53cde233
JanMik19 <j.mikulevic@gmail.com>
2020-12-16 15:04:24 +0000

    change 1

--- zal/foo.hpp
+++ zal/foo.hpp
@@ -1,16 +1,23 @@
-#pragma once
-
 #include "Human.hpp"
-
 #include <algorithm>
 #include <list>
 #include <vector>
 
-std::vector< char > foo(std::list< Human >& people)
+using namespace std;
+
+vector< char > foo(list< Human >& people)
 {
-    std::vector< char > ret_v(people.size());
+  vector< char > ret_v(people.size());
+    
+  for_each(people.begin(), people.end(), [](Human& h) { h.birthday(); });
 
-    // Twoja implementacja tutaj
+  transform(people.rbegin(), people.rend(), ret_v.begin() [](Human& h)
+  {
+    if (h.isMonster())
+      return 'N'; 
+    else
+      return 'Y';
+  });
 
-    return ret_v;
+  return ret_v;
 }

a3945b17e020d8e2a7c009ae71a12794cec56064
JanMik19 <j.mikulevic@gmail.com>
2020-12-16 15:26:20 +0000

    proba1

--- zal/foo.hpp
+++ zal/foo.hpp
@@ -8,16 +8,17 @@ using namespace std;
 vector< char > foo(list< Human >& people)
 {
   vector< char > ret_v(people.size());
-  auto h = [](Human& hum) { hum.birthday(); };
-  for_each(people.begin(), people.end(), h);
 
-  transform(people.rbegin(), people.rend(), ret_v.begin(), [](Human& h)
-  {
+  auto h = [](Human& hum) 
+  { 
+    hum.birthday();
     if (hum.isMonster())
-      return 'N'; 
+      return 'n';
     else
-      return 'Y';
-  });
+      return 'y'; 
+  };
+  
+  transform (people.rbegin(), people.rend(), ret_v.begin(), h);
 
   return ret_v;
 }
